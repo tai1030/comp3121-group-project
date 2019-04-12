6441ab83d6555d932e1a524beb0de5f3c56a69f1
Tai Lam <tai.lam@innoverz.com>
2019-04-13 06:47:46 +0800

    Change default port to 8080 from 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const nextRoutes = require("next-routes");
 
 const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = next({ dev: true });
 
